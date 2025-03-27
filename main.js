afddf607c3fb0b1c384d96c11581f64074096676
n-pontes <nuno06.np@gmail.com>
2025-03-27 11:15:05 +0100

    Add a 2D array that represents a 3x3 grid, add players

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ import Player from './player.js';
 // Create the game board and an empty array
 
 const gameBoard = {
-    game: []
+
+    // Create empty 2D array that represents a 3x3 grid with empty strings for each space
+    game: [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+
+    ]
 };
+
+// Create both players
+const playerA = Player('Player A', 'X');
+const playerB = Player('Player B', 'o');
 
