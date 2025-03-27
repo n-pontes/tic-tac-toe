4915860bdeabdf8a5c47a2b32c38b7598f01dae6
n-pontes <nuno06.np@gmail.com>
2025-03-27 12:15:29 +0100

    Test the function if it works as expected - Fix typo in the printBoard function, missing a '.'

--- main.js
+++ main.js
@@ -19,8 +19,11 @@ const playerB = Player('Player B', 'O');
 
 // Function to print the game board
 const printBoard = () => {
-    console.log(`${gameBoard.board[0]} | ${gameBoardboard[1]} | ${gameBoard.board[2]}`);
+    console.log(`${gameBoard.board[0]} | ${gameBoard.board[1]} | ${gameBoard.board[2]}`);
     console.log(`${gameBoard.board[3]} | ${gameBoard.board[4]} | ${gameBoard.board[5]}`);
     console.log(`${gameBoard.board[6]} | ${gameBoard.board[7]} | ${gameBoard.board[8]}`);
 }
+
+// Test the function
+printBoard();
 
