7203322d99edfb34ad69f8f7a3c335b22fa654bc
n-pontes <nuno06.np@gmail.com>
2025-03-27 12:05:41 +0100

    Add new function printBoard - Print the game board in the browser console - Test it before implementing the game with DOM manipulation - Rename gameBoard parameter to board

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import Player from './player.js';
 const gameBoard = {
 
     // Create empty 2D array that represents a 3x3 grid with empty strings for each space
-    game: [
+    board: [
         ['', '', ''],
         ['', '', ''],
         ['', '', '']
@@ -15,5 +15,12 @@ const gameBoard = {
 
 // Create both players
 const playerA = Player('Player A', 'X');
-const playerB = Player('Player B', 'o');
+const playerB = Player('Player B', 'O');
+
+// Function to print the game board
+const printBoard = () => {
+    console.log(`${gameBoard.board[0]} | ${gameBoardboard[1]} | ${gameBoard.board[2]}`);
+    console.log(`${gameBoard.board[3]} | ${gameBoard.board[4]} | ${gameBoard.board[5]}`);
+    console.log(`${gameBoard.board[6]} | ${gameBoard.board[7]} | ${gameBoard.board[8]}`);
+}
 
